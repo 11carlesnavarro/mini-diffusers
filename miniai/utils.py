288dcddb5dfbd028523e78a41b590c80cb78125c
Carles Navarro Ramírez <carles@OrdenaddeCarles.technicolor.net>
2024-01-31 23:00:56 +0100

    utils needed

---
+++ miniai/utils.py
@@ -0,0 +1,25 @@
+import torch
+from typing import Mapping
+from torch.utils.data import default_collate
+
+def_device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def to_device(x, device=def_device):
+    if isinstance(x, torch.Tensor): 
+        return x.to(device)
+    if isinstance(x, Mapping): 
+        return {k:v.to(device) for k,v in x.items()}
+    
+    return type(x)(to_device(o, device) for o in x)
+
+def to_cpu(x):
+    if isinstance(x, Mapping):
+        return {k:to_cpu(v) for k,v in x.items()}
+    if isinstance(x,list):
+        return [to_cpu(o) for o in x]
+    if isinstance(x, tuple):
+        return tuple(to_cpu(list(x)))
+    return x.detach().cpu()
+
+def collate_device(b): 
+    return to_device(default_collate(b))
